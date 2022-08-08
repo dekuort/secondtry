6a2b0097350198b989c72ae309709d2a658fec58
D3ku0rt <dekuort@icloud.com>
2022-08-08 23:41:57 +0500

    add patient form and project structure conception

--- src/secondtry/web.clj
+++ src/secondtry/web.clj
@@ -7,7 +7,9 @@
             [secondtry.core :as stryc]
             [clojure.pprint]
             [hiccup.table :as ht]
-            [cheshire.core :as cc]))
+            [hiccup.form :as form]
+            [cheshire.core :as cc]
+            [ring.util.anti-forgery :as anti-forgery]))
 
 (defn map-tag [tag xs]
   (map (fn [x] [tag x]) xs))
@@ -16,7 +18,7 @@
    (let [patient-list (cc/parse-string (stryc/get-patient-list) true)]
     (page/html5 {:status 200}
               [:head
-               [:title "Hello World"]]
+               [:title "HealthSamurai Test Task"]]
               [:body 
                (ht/to-table1d patient-list [:first_name "First Name" 
                                              :mid_name "Mid Name" 
@@ -24,8 +26,39 @@
                                              :sex "Sex" 
                                              :birthday "Birthday" 
                                              :address "Address" 
-                                             :insurance_number "Insurance"])
-               [:div {:id "content"} "Hello World"]])))
+                                             :insurance_number "Insurance"]
+                              {:table-attrs {:class "patients-table"}
+                               :th-attrs (fn [label-key _] {:class (name label-key)})})
+               [:br]
+              [:div {:id "add-patient-form"}
+               (form/form-to [:post "/api/test"]
+                             (anti-forgery/anti-forgery-field)
+                             (form/label "add-patient" "Fill a form for add patient information")
+                             [:br]
+                             "First name: "
+                             (form/text-field "first-name")
+                             [:br]
+                             "Midle name: "
+                             (form/text-field "middle-name")
+                             [:br]
+                             "Last name: "
+                             (form/text-field "last-name")
+                             [:br]
+                             "Sex: "
+                             (form/text-field "sex")
+                             [:br]
+                             "Birthday"
+                             (form/text-field "birthday")
+                             [:br]
+                             "Address: "
+                             (form/text-field "address")
+                             [:br]
+                             "Insurance number: "
+                             (form/text-field "insurance-number")
+                             [:br]
+                             (form/submit-button "Add" )
+                             )]
+               ])))
 
 (defn nf []
   (page/html5
@@ -50,7 +83,8 @@
      :headers {"Content-Type" "application/json"}
      :body (stryc/get-patient-list)})
   (POST "/api/add-patient" request (stryc/add-patient (slurp (:body request))))
-  (POST "/api/delete-patient" request (stryc/delete-patient (slurp (:body request)))) 
+  (POST "/api/delete-patient" request (stryc/delete-patient (slurp (:body request))))
+ (POST "/api/test" request (slurp (:body request))) 
   (route/not-found (nf)))
 
 (defn -main []
